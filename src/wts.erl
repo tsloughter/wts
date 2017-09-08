6eac7f65b06a34205d4711f07dd0633b2e07386f
Tristan Sloughter <t@crashfast.com>
2017-09-07 18:51:27 -0700

    clean up formatting in wts doc comments

--- src/wts.erl
+++ src/wts.erl
@@ -1,6 +1,7 @@
 %%--------------------------------------------------------------------
 %% @doc Timestamp support functions.
-%% This library is for use with the suggested Erlang VM args for time warp mode and time correction:
+%% This library is for use with the suggested Erlang VM args for time
+%% warp mode and time correction:
 %%
 %% <pre><code>
 %% $ cat config/vm.args
@@ -9,11 +10,20 @@
 %% +C multi_time_warp
 %% </code></pre>
 %%
-%% These settings require that you have only <a href="http://erlang.org/doc/apps/erts/time_correction.html#Time_Warp_Safe_Code">Time Warp Safe Code</a>.
+%% These settings require that you have only
+%% <a href="http://erlang.org/doc/apps/erts/time_correction.html#Time_Warp_Safe_Code">Time Warp Safe Code</a>.
 %%
-%% Time correction is enabled by Erlang by default if it supported by your system, while the default time warp mode is `no_time_warp' because this is how the runtime behaved before the addition of time warp modes.
+%% Time correction is enabled by Erlang by default if it supported by
+%% your system, while the default  time warp mode is `no_time_warp'
+%% because this is how the runtime behaved before the addition of time
+%% warp modes.
 %%
-%% Example usage to find the time elapsed while performing some function and converting to a timestamp that is in microseconds since the epoch, so has meaning outside of the Erlang runtime. The `wts' timestamp uses the Erlang runtime's `native' time unit for the best accuracy and precision and only converts to microseconds when returning the result of `duration/2' and `to_absolute/1'.
+%% Example usage to find the time elapsed while performing some function
+%% and converting to a timestamp that is in microseconds since the epoch,
+%% so has meaning outside of the Erlang runtime. The `wts' timestamp
+%% uses the Erlang runtime's `native' time unit for the best accuracy
+%% and precision and only converts to microseconds when returning the
+%% result of `duration/2' and `to_absolute/1'.
 %%
 %% <pre><code>
 %% > StartTime = wts:timestamp().
